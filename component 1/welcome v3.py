e0692077f74d11801eb841d5f175ca2f44c5bced
spicyjuicebox <iamshoock@gmail.com>
2023-06-14 11:43:46 +1200

    Continued Component 1 (welcome v3).
    Created the welcome v3.py file. Removed the BTS logo before the greeting.

---
+++ component 1/welcome v3.py
@@ -0,0 +1,62 @@
+import random # Importing the random module. Provides function for generating random numbers and choosing something random from the selection.
+from random import randint # Choosing random integer.
+
+
+# LIST OF RANDOM NAMES ~~~~~~~~~~~~~~
+# The line below is a list for 'names' with the sequence of strings for each name.
+names = ["Jungkook", "Jimin", "V", "J-Hope", "Suga", "RM", "Jin", "Jaehyun", "Felix", "Sunghoon"]
+
+
+# This is a function for the welcome part of the main program.
+# This function is made to generate a random name from the 'names' list above, then print out a welcome message.
+# The welcome message will incluce the selected, randomly generated name.
+def welcome(): # This new function is defined as 'welcome'.
+    # Purpose of what this function is for.
+    '''
+    Purpose: To generate a random name from the list and print out
+    a welcome message.
+    Parameters: None.
+    Returns: None.
+    '''
+    num = randint(0,9) # 0 is the lower bound, 9 is the upper bound. Generates a random number between 0 and 9.
+    name = (names[num]) # Accessing the 'names' list to assign each with a number from 'num'.
+    # START PRINTING THE GREETING ~~~~~~~~~~~~~~
+    print("2.. 3.. Hello, we are BTS!")
+    print("Thank you for choosing...")
+    print(r"""
+
+    _______      ____    ,---.   .--.  .-_'''-. ,---------.    ____    ,---.   .--.         _______       ,-----.  ,---------.  
+    \  ____  \  .'  __ `. |    \  |  | '_( )_   \\          \ .'  __ `. |    \  |  |        \  ____  \   .'  .-,  '.\          \ 
+    | |    \ | /   '  \  \|  ,  \ |  ||(_ o _)|  '`--.  ,---'/   '  \  \|  ,  \ |  |        | |    \ |  / ,-.|  \ _ \`--.  ,---' 
+    | |____/ / |___|  /  ||  |\_ \|  |. (_,_)/___|   |   \   |___|  /  ||  |\_ \|  |        | |____/ / ;  \  '_ /  | :  |   \    
+    |   _ _ '.    _.-`   ||  _( )_\  ||  |  .-----.  :_ _:      _.-`   ||  _( )_\  |        |   _ _ '. |  _`,/ \ _/  |  :_ _:    
+    |  ( ' )  \.'   _    || (_ o _)  |'  \  '-   .'  (_I_)   .'   _    || (_ o _)  |        |  ( ' )  \: (  '\_/ \   ;  (_I_)    
+    | (_{;}_) ||  _( )_  ||  (_,_)\  | \  `-'`   |  (_(=)_)  |  _( )_  ||  (_,_)\  |        | (_{;}_) | \ `"/  \  ) /  (_(=)_)   
+    |  (_,_)  /\ (_ o _) /|  |    |  |  \        /   (_I_)   \ (_ o _) /|  |    |  |        |  (_,_)  /  '. \_/``".'    (_I_)    
+    /_______.'  '.(_,_).' '--'    '--'   `'-...-'    '---'    '.(_,_).' '--'    '--'        /_______.'     '-----'      '---'    
+                                                                                                                                
+
+    """)
+    print("We are a trusted chat bot that will help you make online orders for BTS merch!")
+    print("My name is",name,"!") # Print line includes the randomly selected name from the list and prints it out.
+    print("I will be here to help you make your online orders!")
+
+
+# This is where I am defining the main function of the main program.
+# This function is going to be responsible for running the whole program.
+# This is where I will be including the 'welcome()' function as I will run this in the main program.
+# This function is defined as 'main()'.
+def main():
+    # Purpose of the main() function.
+    '''
+    Purpose: To run all functions.
+    Parameters: None.
+    Returns: None.
+    '''
+    welcome() # Action being executed (the 'welcome()' function. Called within the 'main()' function.
+
+
+# Call the main function.
+# Since the welcome() function was called within the main() function, then this main() function will
+# - execute the welcome() function when called.
+main()
