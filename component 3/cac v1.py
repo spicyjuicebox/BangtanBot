85e5404818abc3c9ef52f5268e409bb6ba2a428c
spicyjuicebox <iamshoock@gmail.com>
2023-07-03 19:49:41 +1200

    Continued Component 3 (cac v1).
    Created the variables, name and phone. Found a bug that the program is accepting anything (nothing, number for name, letter for phone, different characters).

---
+++ component 3/cac v1.py
@@ -0,0 +1,19 @@
+# Bug - Accepts blank input for both name and phone.
+
+
+# Print statement letting the user know that they have to enter in their personal details next.
+print("Please enter your customer information for your Click and Collect order.")
+
+
+# Customer Name
+# What the user enters in this input line of code will be assigned to the variable, 'name'.
+name = input("Please enter your name. ")
+
+
+# Customer Phone Number
+# What the user enters in this input line of code will be assigned to the variable, 'phone'.
+phone = input("Please enter your phone number. ")
+
+
+print(name) # What the user enters for the name input code will be printed here.
+print(phone) # What the user enters for the phone input code will be printed here.
