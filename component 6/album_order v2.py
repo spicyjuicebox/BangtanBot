386ea74fca8e7e029f481cbc160a2924b008944b
spicyjuicebox <iamshoock@gmail.com>
2023-07-04 16:35:24 +1200

    Started Component 6 (album_order v1 & v2).
    Created the album_order v1.py file. Added the album choice system.
    
    Created the album_order v2.py file.
    
    Edited the album_list v2.py file.

--- component 6/album_order v2.py
+++ component 6/album_order v2.py
@@ -4,7 +4,6 @@
 album_names = ['2 Cool 4 Skool','O!RUL8,2?','Skool Luv Affair','No More Dream','The Most Beautiful Moment In Life Pt.1','The Most Beautiful Moment In Life Pt.2','The Most Beautiful Moment In Life: Young Forever','WINGS','Love Yourself: Her','Love Yourself: Tear','Love Yourself: Answer','Map of the Soul: Persona','Map of the Soul: 7','BE','Butter']
 # The 2 Cool 4 Skool album is at position 0.
 
-
 # Another list variable that is named, 'album_prices'.
 # The list is defined using the square brackets, '[]'. Inside these are strings that are separated by commas and each represent the price of each of the albums to be sold.
 # The order of the prices or numbers in this list will be written out in the same order when printed and aligns itself with the album_names list.
@@ -12,7 +11,15 @@ album_prices = [25.70, 32.20, 28.40, 40.20, 40.10, 45.70, 62.50, 45.80, 50.50, 5
 # The first 25.70 is at position 0.
 # This lines up with the album_names of '2 Cool 4 Skool' with it being at position 0.
 
+# List of stored ordered albums.
+order_list = []
 
+# List of stored pizza prices.
+order_cost = []
+
+# List to store order cost.
+
+# Bringing in the albums() function from the previous component.
 # Defining the function for the albums as albums().
 # This function will be responsible for displaying the album names, album prices and their corresponding index numbers.
 def albums():
@@ -28,5 +35,40 @@ def albums():
         # The album_prices[count] will retrieve the price of the album at the current index, 'count', from the 'album_prices' list.
         # The output will be printed out through the format, "{album_number} {album_name} ${album_price}".
 
-# Calling the albums() function to check if the album_list component is working properly.
+# Calling the menu() function to check if the album_list component is working properly.
 albums()
+
+
+# Ask for total number of albums for order.
+num_albums = 0 # This variable has the value of 0.
+
+# The program asks the user how many albums they want to order with the 'input' statement and takes it in as a string.
+# The 'int' is used to convert the input of the user into an integer.
+# The integer will then be stored to the 'num_albums' variable.
+num_albums = int(input("How many albums do you want to order? "))
+
+# Prints out the value of the 'num_albums' variable after the user had entered in their chosen amount.
+print(num_albums)
+
+
+# Choose album from the album list.
+print("Please choose the albums you would like to order by entering its corresponding number from the list provided. ")
+# Used the 'for' statement to create a loop for the number of items that the user has chosen, found in num_albums.
+for item in range(num_albums): # Counting through how many albums have been chosen.
+    # Another loop is made with the 'while' statement to ensure that the user can enter their chosen number.
+    while num_albums > 0:
+        album_ordered = int(input()) # Choosing number of albums from the album list. The input is taken as an integer.
+        album_ordered = album_ordered -1 # The program will subtract 1 from the entered number of albums that the user wants to order. This is to match the index number of the album list.
+        order_list.append(album_names[album_ordered]) # Picking out the album name from 'album_names' and is entered into the 'order_list'.
+        order_cost.append(album_prices[album_ordered]) # Picking out the album price from 'album_prices' and is entered into the 'order_cost'.
+        print("{} ${:.2f}" .format(album_names[album_ordered],album_prices[album_ordered]))
+        # The selected albums and their names and prices will be displayed through the format, "Album name $Album price".
+        num_albums = num_albums-1 # Ordered the first album, going back to order the rest of them, taking 1 off that total.
+
+#print(order_list)
+#print(order_cost)
+
+# Countdown until all albums are ordered
+
+
+# Print order
